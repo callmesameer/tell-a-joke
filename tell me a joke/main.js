c592f04a7af861bc6f8b696af3ce410e45ec4c8c
callmesameer <153595292+callmesameer@users.noreply.github.com>
2024-02-07 17:02:59 +0530

    Add files via upload

---
+++ tell me a joke/main.js
@@ -0,0 +1,215 @@
+let joke = [
+    {
+      "type": "general",
+      "setup": "naaku  koncham thikkundhi kani dhaniko lekkundhi",
+      "punchline": "Dam."
+    },
+    {
+      "type": "general",
+      "setup": "Okadu naaku  edurochina vadike risku.. \n nen vadiki edurellina vadike risk.. \n thokki padestha naa kodaka",
+      "punchline": "You put a little boogie on it."
+    },
+    {
+      "type": "general",
+      "setup": "Nenedi modalupettanu.. \n modalupedithe vadili patanuuu...",
+      "punchline": "1Forrest1"
+    },
+    {
+      "type": "general",
+      "setup": "samayam ledu mithrama.. \n saranama.. ranama...",
+      "punchline": "A waist of time."
+    },
+    {
+      "type": "general",
+      "setup": "Kondaru kodithe X-ray lo kanabaduthundi.. \n inkondaru kodithe Scanning lo kanabaduthundi..\n nenu kodithe History lo vinapaduthindi.",
+      "punchline": "They are two tired"
+    },
+    {
+      "type": "general",
+      "setup": "chudu..oka vaipe chudu..\n rendo vaipu chudalanukoku..\n thattukolevv.. maadipothav..",
+      "punchline": "It goes chew, chew"
+    },
+    {
+      "type": "general",
+      "setup": "Kumara swami..gopala swami..subhramanya swami.. \n Narayana swami..seetharama swami...aiduguru \n kodukulu puttinatte pothunte..eesari puttevadu,\n champe vadu kavali gani chache vadu kakudani \n.mokki maree patteadu ra.. naa peru LAKSHMI NARASIMHA SWAMI ani..",
+      "punchline": "A Dell"
+    },
+    {
+      "type": "general",
+      "setup": "Shh.. sound cheyaku.. kantam kosesthaa..",
+      "punchline": "Tulips"
+    },
+    {
+      "type": "general",
+      "setup": "place nuvvu cheppina sare..\n nannu cheppamanna sare..time nuvvu cheppina sare..\n nannu cheppamanna sare..Kathulatho kaaduraa..\n kanti chuputho champestaa...",
+      "punchline": "You planet"
+    },
+    {
+      "type": "general",
+      "setup": "Evadu kodithe dimma thirigi mind block aypothado… aade pandugadu",
+      "punchline": "Sneakers"
+    },
+    {
+      "type": "general",
+      "setup": "Nenu Mellaga elagola bathikeyyataniki Raledhu....Mumbai ni Ucha Poinchataniki vachanu.",
+      "punchline": "Tooth hurty."
+    },
+    {
+      "type": "knock-knock",
+      "setup": "Nenu kodithe adola untnudi ani allueellu cheppatame kani naaku kuda teliyadhu...ippudu meeku telusthundi.",
+      "punchline": "Never mind. It's pointless."
+    },
+    {
+      "type": "knock-knock",
+      "setup": "Gun ni choodalanuko yappuleddu .... kani bullet ni choodalanukoku chachipothav.",
+      "punchline": "No, cows go moo."
+    },
+    {
+      "type": "knock-knock",
+      "setup": "Athiga aasa pade adadhi athiga avesa pade magaadu suka padi nattu charitra lo ledhu",
+      "punchline": "I didn't know you could yodel!"
+    },
+    {
+      "type": "programming",
+      "setup": "veera sankar reddy mokkey kadha ani peekesthe peeka kostha",
+      "punchline": "Even if you're wrong, you're only off by a bit."
+    },
+    {
+      "type": "programming",
+      "setup": "Aravaku ammathodu Addanga narikestha",
+      "punchline": "Inheritance"
+    },
+    {
+      "type": "programming",
+      "setup": "vaadu pothe veedu veedu pothe nenu, nenu pothe naa amma mogudu antu evadaina adhikaram kosam yegapadithe ",
+      "punchline": "The Foo Bar."
+    },
+    {
+      "type": "programming",
+      "setup": "Kantapaddava Kanikaristanemo… Ventapaddava Narikesta Oba",
+      "punchline": "Because he didn't get arrays."
+    },
+    {
+      "type": "general",
+      "setup": " Niku ego lopale untundhemo, naku na chuttu WIFI la untundi",
+      "punchline": "It ended in a tie."
+    },
+    {
+      "type": "general",
+      "setup": "Dikki balisina kodi, chicken shop ki vachi thoda kottindhanta",
+      "punchline": "A Yamahahahaha."
+    },
+    {
+      "type": "general",
+      "setup": "Nee target 10 miles aithe aim for the 11nth mile",
+      "punchline": "'Where is the bar tended?'"
+    },
+    {
+      "type": "general",
+      "setup": " cheyyi chusava entha rough ga undo… roughaadinchesta",
+      "punchline": "Narnia business!"
+    },
+    {
+      "type": "programming",
+      "setup": "Nenu alochincha kunda matladanu.. Matladinaka alochinchanu.. Gatam Gatam.",
+      "punchline": "Because Oct 31 == Dec 25"
+    },
+    {
+      "type": "programming",
+      "setup": "Naa daari rahadaari",
+      "punchline": "'Can I join you?'"
+    },
+    {
+      "type": "programming",
+      "setup": "How many programmers does it take to change a lightbulb?? Evadiki Telsu  Ra Evadiki Telsu..",
+      "punchline": "None that's a hardware problem"
+    },
+    {
+      "type": "programming",
+      "setup": "If you put a million monkeys at a million keyboards, one of them will eventually write a Java program ,fuck im kidding",
+      "punchline": "the rest of them will write Perl"
+    },
+    {
+      "type": "programming",
+      "setup": "['hip', 'hip' array]",
+      "punchline": "(hip hip array)"
+    },
+    {
+      "type": "programming",
+      "setup": "To understand what recursion is..You must first understand what recursion is. ",
+      "punchline": "You must first understand what recursion is"
+    },
+    {
+      "type": "programming",
+      "setup": "There are 10 types of people in this world...Those who understand binary and those who don't",
+      "punchline": "Those who understand binary and those who don't"
+    },
+    {
+      "type": "general",
+      "setup": "Avakasham Kosam Darlu Tokke Paatrale Tappa Herolu Villanlu Leree Natakamlo",
+      "punchline": "Put it on my bill"
+    },
+    {
+      "type": "general",
+      "setup": "Bhayapadevaade Beranikostadu Mana Daggara Beralevamma",
+      "punchline": "It gets toad away"
+    },
+    {
+      "type": "general",
+      "setup": "Neeku BP Vaste Nee PA Vanukutaademo, Naaku BP Vaste AP Vanukuddi",
+      "punchline": "they were cooked in Greece"
+    },
+    {
+      "type": "programming",
+      "setup": "Ooru Maarite Tine Food Maaruddi Padukune Bed Maaruddi Blood Enduku Maaruddi Ra Bloody Fool",
+      "punchline": "Can't catch me - Avicii"
+    },
+    {
+      "type": "knock-knock",
+      "setup": "Sar Sarle Enno Anukuntam… Anni Avutaya Enti",
+      "punchline": "That is impossible. Opportunity doesn’t come knocking twice!"
+    },
+    {
+      "type": "programming",
+      "setup": "Why do Java programmers wear glasses? Because they don't C#. That programmer says baney extralu dengutunnav ga",
+      "punchline": "Because they don't C#."
+    },
+    {
+      "type": "general",
+      "setup": "Why did the mushroom get invited to the party... evadiki telsu ra evadiki telsu",
+      "punchline": "Because he was a fungi."
+    },
+    {
+      "type": "general",
+      "setup": "Tappu Naa Vaipu Undi Kabatti Tala Dinchukuni Veltunna, Ledante Talalu Teesukellevaadni",
+      "punchline": "Before was was was was was is."
+    },
+    {
+        "type": "general",
+        "setup": "jai balayya",
+        "punchline": "Before was was was was was is."
+      },
+      {
+        "type": "general",
+        "setup": "nee kanti choopulloki naa pranam cherindhe ea maya chesavey",
+        "punchline": "Before was was was was was is."
+      },
+      {
+        "type": "general",
+        "setup": "okkokkadni kad sharkhan vandha mandhini oke sari rammanu lekka thakkuvaina parledhu ekkuva kakunda chusko",
+        "punchline": "Before was was was was was is."
+      },
+      {
+        "type": "general",
+        "setup": "Satruvulu Ekkado Undarra, Chellellu Kooturla Roopamlo Maaruveshalesukuni Mana Kompa Lone Tiruguta Untaru",
+        "punchline": "Okkokadni kadhu sharkhan 100 mandhini okesari rammanu ...gu**a de*gesta"
+      },
+  ]
+  
+function myfun(){
+    let vj = Math.floor(Math.random() *joke.length);
+    let jokes=document.getElementById("jokes");
+    jokes.innerText=joke[vj].setup;
+    console.log(vj);
+
+}
